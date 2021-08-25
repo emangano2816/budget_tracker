e68eaa687724dc48b4cdb8023cfcc3350df9f21f
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-25 08:34:58 -0400

    Added cache variables and code to install sw.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
-console.log("Hello from your service worker.");
+//add variables to store cache
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+//install event handler
+self.addEventListener("install", function(event) {
+    //pre cache static assets
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    //activate service worker immediately after install complete
+    self.skipWaiting();
+});
+
+
